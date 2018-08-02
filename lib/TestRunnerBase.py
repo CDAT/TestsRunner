78a36aec3cf2520fff950504b52a55a3870f2aa4
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-08-02 08:39:17 -0700

    replaced some of  TestRunnerBase's __get* method names with _get* since we need to call it from a subclass for implementing --subdir option

--- lib/TestRunnerBase.py
+++ lib/TestRunnerBase.py
@@ -72,7 +72,7 @@ class TestRunnerBase(object):
             download_sample_data_files(
                 test_data_files_info, get_sampledata_path())
 
-    def __get_tests(self, tests=None):
+    def _get_tests(self, workdir, tests=None):
         """
         get_tests() gets the list of test names to run.
         If <failed_only> is False, returns the set of the specified
@@ -85,7 +85,7 @@ class TestRunnerBase(object):
         """
         if tests is None or len(tests) == 0:
             # run all tests
-            test_names = glob.glob("tests/test_*py")
+            test_names = glob.glob("{w}/tests/test_*py".format(w=workdir))
         else:
             test_names = set(tests)
 
@@ -102,7 +102,7 @@ class TestRunnerBase(object):
 
         return test_names
 
-    def __get_baseline(self, workdir):
+    def _get_baseline(self, workdir):
         """
         __get_baseline(self, workdir):
         <workdir> : should be repo dir of the test
@@ -136,7 +136,7 @@ class TestRunnerBase(object):
         """Place holder extend this if you want more options"""
         return []
 
-    def __do_run_tests(self, test_names):
+    def _do_run_tests(self, workdir, test_names):
         ret_code = SUCCESS
         p = multiprocessing.Pool(self.ncpus)
         # Let's prep the options once and for all
@@ -255,7 +255,7 @@ class TestRunnerBase(object):
         print("<tfoot><tr><th>Test</th><th>Result</th><th>Start Time</th>"
               "<th>End Time</th><th>Time</th></tr></tfoot>", file=fh)
 
-    def __generate_html(self, workdir, image_difference=True):
+    def _generate_html(self, workdir, image_difference=True):
         os.chdir(workdir)
         if not os.path.exists("tests_html"):
             os.makedirs("tests_html")
@@ -334,7 +334,7 @@ class TestRunnerBase(object):
         os.chdir(workdir)
         webbrowser.open("file://%s/tests_html/index.html" % workdir)
 
-    def __package_results(self, workdir):
+    def _package_results(self, workdir):
         os.chdir(workdir)
         import tarfile
         tnm = "results_%s_%s_%s.tar.bz2" % (
@@ -355,19 +355,19 @@ class TestRunnerBase(object):
         tests  : a space separated list of test cases
         """
         os.chdir(workdir)
-        test_names = self.__get_tests(self.args.tests)
+        test_names = self._get_tests(workdir, self.args.tests)
 
         if self.args.checkout_baseline:
-            ret_code = self.__get_baseline(workdir)
+            ret_code = self._get_baseline(workdir)
             if ret_code != SUCCESS:
                 return(ret_code)
 
-        ret_code = self.__do_run_tests(test_names)
+        ret_code = self._do_run_tests(workdir, test_names)
 
         if self.args.html or self.args.package:
-            self.__generate_html(workdir)
+            self._generate_html(workdir)
 
         if self.args.package:
-            self.__package_results(workdir)
+            self._package_results(workdir)
 
         return ret_code
