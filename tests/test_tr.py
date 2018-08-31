09ad0ebba6fee095886bf3fa6ff697cd774aaf3a
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-08-31 09:33:48 -0700

    move test_options_file.json out of share

--- tests/test_tr.py
+++ tests/test_tr.py
@@ -23,9 +23,9 @@ class TestTestRunnerBase(unittest.TestCase):
         workdir = os.path.join(os.path.dirname ( __file__), os.path.pardir)
         testsuite_name = "testsrunner"
         options_files = []
-        options_files.insert(0, os.path.join(
-                sys.prefix, "share", "testsrunner", "test_options_file.json"))
-        options = [ "-s", "--with-coverage", "--cover-html", "--cover-xml", "--cover-package"]
+        option_file = os.path.join(this_dir, '..', 'resources', 'test_options_file.json')
+        options_files.insert(0, option_file)
+        options = [ "-s", "--with-coverage", "--cover-html", "--cover-xml", "--cover-package" ]
         runner = TestRunnerBaseSubClass(testsuite_name, options=options,
                                         options_files=options_files)
         ret_code = runner.run(workdir, tests="tests/test_tr_flake8.py")
