a5d7fc5018c153a0d6906deb69d150501d1d4342
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-09-26 16:27:45 -0700

    cleanup obsolete code

--- tests/test_tr.py
+++ tests/test_tr.py
@@ -45,11 +45,6 @@ class TestTestRunnerBase(unittest.TestCase):
         ret_code = self.runner.run(workdir, tests="tests/test_passing_test.py")
         self.assertEqual(ret_code, 0)
 
-    #def testGetBaseline(self):
-    #    workdir = os.path.join(os.path.dirname ( __file__), os.path.pardir)
-    #    ret_code = self.runner._get_baseline(workdir)
-    #    self.assertEqual(ret_code, 0)
-
     def testGenerateHtml(self):
         workdir = os.path.join(os.path.dirname ( __file__), os.path.pardir)
         ret_code = self.runner.run(workdir, tests="tests/test_passing_test.py")
@@ -67,27 +62,7 @@ class TestTestRunnerBase(unittest.TestCase):
         ret_code = self.runner._package_results(workdir)
         self.assertEqual(ret_code, 0)
 
-    def PREVtestBaselinesPREV(self):
-        testsuite_name = "testsrunner"
-        options_files = []
-        option_file = os.path.join(this_dir, '..', 'tests', 'test_options_file.json')
-        options_files.insert(0, option_file)
-        options = [ "-s", "--with-coverage", "--cover-html", "--cover-xml", "--cover-package", "--coverage", "--checkout-baseline" ]
-        workdir = os.path.join(os.path.dirname ( __file__), os.path.pardir)
-        test_data_files = os.path.join(this_dir, '..', 'tests', 'test_data_files.txt')
-        self.runner2 = TestRunnerBaseSubClass(testsuite_name, options=options,
-                                              options_files=options_files,
-                                              get_sample_data=True,
-                                              test_data_files_info=test_data_files)
-        with self.assertRaises(Exception) as context:
-            ret_code = self.runner2._get_baseline(workdir)
-        self.runner2.baselines_fallback_on_master = True
-        ret_code = self.runner2._get_baseline(workdir)
-        self.assertEqual(ret_code, 0)
-
-
-    def testBaselines(self):
-
+    def testGetBaselines(self):
         workdir = os.path.join(os.path.dirname ( __file__), os.path.pardir)
         data_dir = os.path.join(workdir, "uvcdat-testdata")
         if os.path.isdir(data_dir):
