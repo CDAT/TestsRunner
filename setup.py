d4205f56f053b4b2e240d12f421d8653b585895c
Charles Doutriaux <doutriaux1@llnl.gov>
2018-11-08 09:23:43 -0800

    give it version based on github

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 from distutils.core import setup
+from subprocess import Popen, PIPE
+
+Version = "1.0"
+p = Popen(
+    ("git",
+     "describe",
+     "--tags"),
+    stdin=PIPE,
+    stdout=PIPE,
+    stderr=PIPE)
+try:
+    descr = p.stdout.readlines()[0].strip().decode("utf-8")
+    Version = "-".join(descr.split("-")[:-2])
+    if Version == "":
+        Version = descr
+except:
+    descr = Version
+
+
 
 scripts = [ 'scripts/print_something.py' ]
 
 
 setup(name="testrunner",
       author="AIMS Software Team",
+      version=descr,
       description="Utilities for running nose-based test suites",
       url="http://github.com/cdat/TestsRunner",
       packages=['testsrunner'],
