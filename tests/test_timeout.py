c970ff489fec84107df377720c4f7d1b901814b7
Charles Doutriaux <doutriaux1@llnl.gov>
2018-12-04 10:24:49 -0500

    forgot to add the tests

---
+++ tests/test_timeout.py
@@ -0,0 +1,9 @@
+import unittest
+import time
+
+class TestTimeout(unittest.TestCase):
+
+    def testTimeout(self):
+        time.sleep(5)
+        print("...testPass...")
+        self.assertEqual(0,0)
